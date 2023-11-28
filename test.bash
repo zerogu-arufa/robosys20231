0755dbc1cd92420c5f78d6ab7cb4a220ce07ca2a
zerogu-arufa <kasutama59@gmail.com>
2023-11-28 14:33:16 +0900

    コマンド追加

--- test.bash
+++ test.bash
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 # SPDX-FileCopyrightText: 2023 Tateuchi
 # SPDX-License-Identifier: BSD-3-Clause
-import sys
-
-ans = 0
-for line in sys.stdin:
-	    try:
-	    ans += int(line)
-	    print(ans)
-
-	    except ValueError:
-	    try:
-	    ans += float(line)
-	    print(ans)
-
-	    except ValueError:
-	    print("入力が変だぞ(￣д￣)")
+ng () {
+	      echo NG at Line $1
+	            res=1
+	    }
+	    res=0
+	    ### I/O TEST ###
+	    out=$(seq 5 | ./plus)
+	    [ "${out}" = 14 ] || ng ${LINENO}
+	    　
+	    [ "$res" = 0 ] && echo OK 
+	    # &&（AND記号）は左側が成功すると右側を実行
+	    exit $res
