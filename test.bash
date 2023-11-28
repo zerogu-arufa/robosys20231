fef48cd0305dc73a498ce933231ca2b76eff3c77
zerogu-arufa <kasutama59@gmail.com>
2023-11-28 15:12:50 +0900

    変更

--- test.bash
+++ test.bash
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 # SPDX-FileCopyrightText: 2023 Tateuchi
 # SPDX-License-Identifier: BSD-3-Clause
-ng () {
-	      echo ${1}行目が違うわボケ！
+ng (){
+	echo ${1}行目が違うわ
 	            res=1
 	    }
-	    res=0
+		    res=0
 	    ### I/O TEST ###
 	    out=$(seq 5 | ./plus)
 	    [ "${out}" = 15 ] || ng ${LINENO}
-	    　
 	    [ "$res" = 0 ] && echo OK 
 	    # &&（AND記号）は左側が成功すると右側を実行
 	    exit $res
