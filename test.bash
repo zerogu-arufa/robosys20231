4e569db739b3176ae13de131800fbfd8d9dbf9a3
zerogu-arufa <kasutama59@gmail.com>
2023-11-29 07:11:58 +0900

    修正

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Tateuchi
 # SPDX-License-Identifier: BSD-3-Clause
 ng (){
@@ -6,10 +6,20 @@ ng (){
 	            res=1
 	    }
 res=0
+### I/O テスト###
 out=$(seq 5 | ./plus)
             [ "${out}" = "15
 -15
 120" ] || ng ${LINENO}
 	    [ "$res" = 0 ] && echo OK 
 	    # &&（AND記号）は左側が成功すると右側を実行
-exit $res
+
+### 例外動作テスト ###
+out=$(echo あ | ./plus)
+[ "$?" = 0 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus) #空文字
+[ "$?" = 0 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+	    exit $res
