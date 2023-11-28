66db67657275ae69d39a549f229f0e61368bde3e
zerogu-arufa <kasutama59@gmail.com>
2023-11-28 15:08:56 +0900

    変更

--- test.bash
+++ test.bash
@@ -2,7 +2,7 @@
 # SPDX-FileCopyrightText: 2023 Tateuchi
 # SPDX-License-Identifier: BSD-3-Clause
 ng () {
-	      echo NG at Line $1
+	      echo ${1}行目が違うわボケ！
 	            res=1
 	    }
 	    res=0
