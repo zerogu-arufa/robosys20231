66ea7a35d34563591ea59b4042a0ffbc2bdb67b7
zerogu-arufa <kasutama59@gmail.com>
2023-11-28 15:52:50 +0900

    コマンド追加

--- test.bash
+++ test.bash
@@ -8,7 +8,9 @@ ng (){
 		    res=0
 	    ### I/O TEST ###
 	    out=$(seq 5 | ./plus)
-	    [ "${out}" = 15 ] || ng ${LINENO}
+	    [ "${out}" = "足し算の結果: 15
+	    引き算の結果: -15
+	    掛け算の結果: 120" ] || ng ${LINENO}
 	    [ "$res" = 0 ] && echo OK 
 	    # &&（AND記号）は左側が成功すると右側を実行
 	    exit $res
