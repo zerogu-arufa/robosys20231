9df8b502227a5d1f8b33405a191ba094962e6078
zerogu-arufa <kasutama59@gmail.com>
2023-11-28 14:19:39 +0900

    command追加

--- test.bash
+++ test.bash
@@ -5,10 +5,21 @@ import sys
 
 ans = 0
 for line in sys.stdin:
+	ans = input("数値を入力してください: ")
+	x   = input("数値を入力してください: ")
+	
 	try:
-	ans += int(line)
-	except:
-	ans += float(line)
+	ans = int(ans)
+	x   = int(x)
+	ans += x
 	print(ans)
+
 	except ValueError:
-	print("入力が変だぞ(￣д￣)") 	
+	try:
+	ans = float(ans)
+	x   = float(ans)
+	ans += x
+	print(ans)
+	
+	except ValueError:
+	print("入力が変だぞ(￣д￣)")  
