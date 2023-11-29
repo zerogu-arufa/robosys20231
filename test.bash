fc08c6f8846f06ea8f4ad8996b4c480cdd319645
zerogu-arufa <kasutama59@gmail.com>
2023-11-29 14:30:23 +0900

    調整

--- test.bash
+++ test.bash
@@ -1,16 +1,16 @@
 #!/usr/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Tateuchi
 # SPDX-License-Identifier: BSD-3-Clause
+
 ng (){
 	echo ${1}行目が違います。
 	            res=1
-	    }
+}
 res=0
+
 ### I/O テスト###
 out=$(seq 5 | ./plus)
-            [ "${out}" = "15
--15
-120" ] || ng ${LINENO}
+[ "${out}" = "15 -15 120" ] || ng ${LINENO}
 	    [ "$res" = 0 ] && echo OK 
 	    # &&（AND記号）は左側が成功すると右側を実行
 
@@ -19,4 +19,4 @@ out=$(echo あ | ./plus)
 [ "$?" = 0 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
             [ "$res" = 0 ] && echo OK
-	    exit $res
+exit $res
