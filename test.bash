a5427d27d9cd2348781c857d368eec727d59e6b3
zerogu-arufa <kasutama59@gmail.com>
2023-11-29 14:35:49 +0900

    調整

--- test.bash
+++ test.bash
@@ -12,13 +12,14 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = "15
 -15
-120" ] || ng ${LINENO}
-	    [ "$res" = 0 ] && echo OK 
-	    # &&（AND記号）は左側が成功すると右側を実行
+120" ]              || ng ${LINENO}
+
+# &&（AND記号）は左側が成功すると右側を実行
 
 ### 例外動作テスト ###
 out=$(echo あ | ./plus)
-[ "$?" = 0 ]                || ng ${LINENO}
-[ "${out}" = "" ]           || ng ${LINENO}
-            [ "$res" = 0 ] && echo OK
+[ "$?" = 0 ]        || ng ${LINENO}
+[ "${out}" = "" ]   || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
 exit $res
